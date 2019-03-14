747c1b62ee2991e25da36a279b4bdfb54c3f3276
Harry <matheuscampelocavalcante@hotmail.com>
2019-03-14 13:14:26 -0300

    adicionado prints, contexto e comentarios

--- segundo-projeto/src/com/stefanini/treinamento/servlet/CachorroServlet.java
+++ segundo-projeto/src/com/stefanini/treinamento/servlet/CachorroServlet.java
@@ -13,10 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.stefanini.treinamento.model.Cachorro;
 @WebServlet("/cachorro")
-public class CachorroServlet extends HttpServlet {
-
+public class CachorroServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("service");
 		super.service(req, resp);
 	}
 	@Override
@@ -25,6 +27,7 @@ public class CachorroServlet extends HttpServlet {
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("get");
 		List<Cachorro> cachorros = new ArrayList<>();
 		cachorros.add(new Cachorro("Bob",2));
 		cachorros.add(new Cachorro("Aroldo",10));
@@ -33,7 +36,9 @@ public class CachorroServlet extends HttpServlet {
 		resp.getWriter().append(dog.getNome()+" "+dog.getIdade()+" ");
 		
 		}
+		resp.getWriter().append("     "+req.getContextPath());
 		//resp.getWriter().append(cachorros.toString());
 		
 	}
+	//Implementar o resto dos metodos HTTP
 }
